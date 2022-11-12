418ff6a5eedfa5eff7a60d350b541d5f37f06db6
Sai Sompally <ssompally@gmail.com>
2022-11-12 18:12:49 -0500

    testing

--- client/react-app/src/pages/UserOutput.js
+++ client/react-app/src/pages/UserOutput.js
@@ -8,17 +8,59 @@ import {item, nearest_recycling_center_name, nearest_recycling_center_addr} from
 import { Wrapper } from "@googlemaps/react-wrapper";
 import Mapping from '../components/Mapping.js';
 
-
-
 // The General Landing Page for all users
 // TODO: We still have to add actions for all buttons (to sign up/login)
 const UserOutput = () => {
+/*
+    function MapDirectionsRenderer(props) {
+        const [directions, setDirections] = useState(null);
+        const [error, setError] = useState(null);
+      
+        useEffect(() => {
+          const { places, travelMode } = props;
+      
+          const waypoints = places.map(p => ({
+            location: { lat: p.latitude, lng: p.longitude },
+            stopover: true
+          }));
+          const origin = waypoints.shift().location;
+          const destination = waypoints.pop().location;
+      
+          const directionsService = new google.maps.DirectionsService();
+          directionsService.route(
+            {
+              origin: origin,
+              destination: destination,
+              travelMode: travelMode,
+              waypoints: waypoints
+            },
+            (result, status) => {
+              console.log(result)
+              if (status === google.maps.DirectionsStatus.OK) {
+                setDirections(result);
+              } else {
+                setError(result);
+              }
+            }
+          );
+        });
+      
+        if (error) {
+          return <h1>{error}</h1>;
+        }
+        return (
+          directions && (
+            <DirectionsRenderer directions={directions} />
+          )
+        );
+      }
+      */
+
     let navigate = useNavigate()
 
     return ( 
         <ThemeProvider theme={theme}>
             <CssBaseline enableColorScheme />
-                
                 
                 <Typography variant="h4" align="center" sx={{margin: 4, color: "black"}}>
                     Nearest Recycling Center: {nearest_recycling_center_name}
