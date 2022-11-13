a6fbe3061ed941142e39bcdcdbd3d5e44d78524f
Sai Sompally <ssompally@gmail.com>
2022-11-13 01:52:46 -0500

    UI changes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const cheerio = require('cheerio');
 const request = require('request');
 const cors = require('cors');
 
+const Clarifai = require('clarifai')
+
 app.use(cors());
 
 
@@ -103,5 +105,64 @@ app.get("/:item/:user_latitude/:user_longitude", cors(), (req, res) => {
     
   })
 })
+
+app.get('/image-predict', (req, res) => {
+  const USER_ID = 'as29bnia3z50';
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '4fc863f052064e70a97dfc5dc15f7dc4';
+    const APP_ID = '5467c54cb7e54d2483e78627401183a5';
+    // Change these to whatever model and image input you want to use
+    const MODEL_ID = 'general-image-recognition';
+    const IMAGE_FILE_LOCATION = 'YOUR_IMAGE_FILE_LOCATION_HERE';
+    // This is optional.You can specify a model version or the empty string for the default
+    const MODEL_VERSION_ID = '';
+
+    const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+    const stub = ClarifaiStub.grpc();
+
+    // This will be used by every Clarifai endpoint call
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", "Key " + PAT);
+
+    const fs = require("fs");
+    const imageBytes = fs.readFileSync(IMAGE_FILE_LOCATION);
+
+    stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version.
+        inputs: [
+            { data: { image: { base64: imageBytes } } }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        // Since we have one input, one output will exist here.
+        const output = response.outputs[0];
+
+        console.log("Predicted concepts:");
+        for (const concept of output.data.concepts) {
+            console.log(concept.name + " " + concept.value);
+        }
+    }
+  );
+})
+
+
+
+
 
 
